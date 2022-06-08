ab746e848c7b294774837659609b82591a94b3e9
Ches-25 <1572484160@qq.com>
2022-06-08 15:41:40 +0800

    新增编辑接口

--- sever.js
+++ sever.js
@@ -3,8 +3,10 @@ const app = express()
 const port = 3001
 const fs = require('fs')
 const cors = require('cors')
+const { type } = require('express/lib/response')
 app.use(cors())
-
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/home/multidata', (req, res) => {
   let f = fs.readFile("./data/home/home_data.json", "utf-8", function (err, data) {
@@ -79,4 +81,86 @@ app.get('/recommend', (req, res) => {
   })
 })
 
-app.listen(port, () => console.log(`Example app listening on port 3001!`))
+app.get('/tableData', (req, res) => {
+  params = req.query
+
+  listType = params[Object.keys(params)[0]]
+  cate = params[Object.keys(params)[1]]
+  page = params[Object.keys(params)[2]]
+
+  let file = `./data/tableData/${listType}/${cate}/page${page}.json`
+  if (listType =='goods') {
+    
+  }
+  let f = fs.readFile(file, "utf-8", function (err, data) {
+    if (err) {
+      res.json(err)
+    } else {
+      let j = JSON.parse(data);
+      res.json(j)
+    }
+  })
+})
+
+app.get('/goods/amount/list', (req, res) => {
+  let f = fs.readFile("./data/statisticData/statistic.json", "utf-8", function (err, data) {
+    let j = JSON.parse(data);
+    // console.log(data);
+    res.json(j)
+    // console.log(j);
+  })
+})
+
+app.post('/table/operate', (req, res) => {
+  let opType = req.query.opType //获取操作类型
+  let listType = req.query.listType //获取列表类型
+  let tabData = req.body //获取前端的数据
+  console.log(req.query,req.body);
+
+  //文件路径
+  let fileUrl = `./data/table/${listType}/data.json`
+  //读取文件
+  fs.readFile(fileUrl, (err, data) => {
+    // 判断操作类型
+    let tableList = JSON.parse(data);
+    if (opType==='add') {
+      tabData.status = '启用',
+      tabData.cfav = (parseInt(tableList.data[0].cfav) + 1) + ''
+      tabData.iid = (parseInt(tableList.data[0].iid) + 1) + ''
+      tabData.createAt = '2022-05-19 10:20:00'
+      tableList.data.unshift(tabData)
+    }
+    if (opType==='detail') {
+      const dataIndex = tableList.data.findIndex(item => item.iid === tabData.iid)
+      tableList.data.splice(dataIndex,1,tabData)
+    }
+    if (opType==='delete') {
+      const dataIndex = tableList.data.findIndex(item => item.iid === tabData.iid)
+      tableList.data.splice(dataIndex,1)
+      if (tableList.data.length===0) {
+        return res.json({success:false,message:"数据不能全部删掉"})
+      }
+    }
+    if (opType==='batch_delete') {
+      for(let iid of tabData) {
+        const dataIndex = tableList.data.findIndex(item => item.iid === iid)
+        tableList.data.splice(dataIndex,1)
+      }
+      if (tableList.data.length===0) {
+        return res.json({success:false,message:"数据不能全部删掉"})
+      }
+    }
+    //重新写入文件
+    fs.writeFile(fileUrl, JSON.stringify(tableList), (err) => {
+      if (err) {
+        console.log(err);
+      }
+      res.json(tableList)
+      console.log('写入成功！');
+    })
+  })
+})
+
+
+
+app.listen(port, () => console.log(`应用启动,监听端口${port}!`))
